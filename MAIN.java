f79c1fa2226e36310286cbc1bbde651049745b90
Johan Magnusson <joma2304@student.miun.se>
2025-10-08 12:58:31 +0200

    first commit

---
+++ MAIN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; //Scanner klassen
+
+public class MAIN {
+
+    //Konstanta variabler
+    public static int DICE_MIN_NR = 1;
+    public static int DICE_MAX_NR = 3;
+    public static int POINTS_TO_WIN = 12;
+    public static void main(final String[] args) {
+
+    //Variabler för tärningarna
+
+    //Summa för alla tärningar tillsammans
+
+    //Resultat (vinst eller förlust)
+
+    }
+}
